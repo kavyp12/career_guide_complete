78c9a950a980a21caad33911c8d8bdc0cb17bab0
kavyp12 <92688681+kavyp12@users.noreply.github.com>
2025-03-03 16:57:19 +0530

    end point

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://16.16.129.30:3000',
         changeOrigin: true
       }
     }
